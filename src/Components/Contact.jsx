e08e5985e264dd8585c28e69faa36e93dc6c0e4a
Kumar Anand <kranand9430@gmail.com>
2025-02-15 04:13:28 +0530

    bug-fix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,9 +11,9 @@ function Contact() {
             <div className='flex flex-col lg:flex-row gap-10 justify-between  items-center m-5'>
             <div className='flex flex-col gap-6 items-center lg:items-start'>
                 <h1 className='text-2xl lg:text-3xl'>Get in Touch :</h1>
-                <p className='flex flex-row items-center text-sm sm:text-base md:text-lg lg:text-xl'>Mail at : &nbsp;<a className='flex flex-row items-center underline text-[#BB86FC] hover:scale-110 transition-all' href="mailto:kranand9430@gmail.com">kranand9430@gmail.com &nbsp;<SiMinutemailer />
+                <p className='flex flex-row items-center text-md md:text-lg lg:text-xl'>Mail at : &nbsp;<a className='flex flex-row items-center underline text-[#BB86FC] hover:scale-110 transition-all' href="mailto:kranand9430@gmail.com">kranand9430@gmail.com &nbsp;<SiMinutemailer />
                 </a></p>
-                <p className='flex flex-row items-center text-sm sm:text-base md:text-lg lg:text-xl'>Contact me : &nbsp;<a className='flex flex-row items-center underline text-[#BB86FC] hover:scale-110 transition-all' href="tel:+917520537469">+91 75205 37469 &nbsp;<IoMdCall />
+                <p className='flex flex-row items-center text-md md:text-lg lg:text-xl'>Contact me : &nbsp;<a className='flex flex-row items-center underline text-[#BB86FC] hover:scale-110 transition-all' href="tel:+917520537469">+91 75205 37469 &nbsp;<IoMdCall />
                 </a></p>
             </div>
             <h1 className='text-5xl m-8'>OR</h1>
